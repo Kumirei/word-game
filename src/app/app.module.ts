a9680d1e270476a738be46681afb530de0c2aa89
Kumirei <linusson.sixten@gmail.com>
2024-05-27 16:12:45 -0400

    Angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,17 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
 import { GameComponent } from '../components/game/game.component'
-import { HttpClientModule } from '@angular/common/http'
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { FormsModule } from '@angular/forms'
 import { Twinkles } from '../components/twinkles/twinkles.component'
 import { StatsComponent } from '../components/stats/stats.component'
 import { ChartComponent } from '../components/chart/chart.component'
 
-@NgModule({
-    declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        HttpClientModule,
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
         FormsModule,
         Twinkles,
         ChartComponent,
         GameComponent,
-        StatsComponent,
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-})
+        StatsComponent], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
